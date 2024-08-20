462f2c90e01f70de98e1b35883fab2ccc3555b24
myhzy <notmyhzy@hotmail.com>
2024-08-20 22:20:20 +0100

    refactor and css changes

--- script.js
+++ script.js
@@ -15,14 +15,6 @@ const tabsContent = document.querySelectorAll(".operations__content");
 
 // const openModal = function (e) {
 //   e.preventDefault();
-//   modal.classList.remove('hidden');
-//   overlay.classList.remove('hidden');
-// };
-
-// const closeModal = function () {
-//   modal.classList.add('hidden');
-//   overlay.classList.add('hidden');
-// };
 
 // btnsOpenModal.forEach(btn => addEventListener('click', openModal));
 
